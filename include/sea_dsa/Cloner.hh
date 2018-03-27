deb187fdb4255076d36212aa7c8a95414d3ff086
Jakub Kuderski <kubakuderski@gmail.com>
2018-03-27 18:26:26 -0400

    Format Cloner

--- include/sea_dsa/Cloner.hh
+++ include/sea_dsa/Cloner.hh
@@ -3,35 +3,32 @@
 
 #include "sea_dsa/Graph.hh"
 
-namespace sea_dsa
-{
-  /** 
-   * Recursively clone DSA sub-graph rooted at a given Node
-     */
-  class Cloner
-  {
-    Graph &m_graph;
-    llvm::DenseMap<const Node*, Node*> m_map;
-    
-  public:
-    Cloner (Graph &g) : m_graph(g) {}
-    
-    /// Returns a clone of a given node in the new graph
-    /// Recursive clones nodes linked by this node as necessary
-    Node &clone (const Node &n);
-    
-    /// Returns a cloned node that corresponds to the given node
-    Node &at (const Node &n)
-    {
-      assert (hasNode (n));
-      auto it = m_map.find (&n);
-      assert (it != m_map.end ());
-      return *(it->second);
-    }
-    
-    /// Returns true if the node has already been cloned
-    bool hasNode (const Node &n) { return m_map.count (&n) > 0; }
-  };
-}
+namespace sea_dsa {
+/**
+ * Recursively clone DSA sub-graph rooted at a given Node
+ */
+class Cloner {
+  Graph &m_graph;
+  llvm::DenseMap<const Node *, Node *> m_map;
+
+public:
+  Cloner(Graph &g) : m_graph(g) {}
+
+  /// Returns a clone of a given node in the new graph
+  /// Recursive clones nodes linked by this node as necessary
+  Node &clone(const Node &n);
+
+  /// Returns a cloned node that corresponds to the given node
+  Node &at(const Node &n) {
+    assert(hasNode(n));
+    auto it = m_map.find(&n);
+    assert(it != m_map.end());
+    return *(it->second);
+  }
+
+  /// Returns true if the node has already been cloned
+  bool hasNode(const Node &n) { return m_map.count(&n) > 0; }
+};
+} // namespace sea_dsa
 
 #endif
