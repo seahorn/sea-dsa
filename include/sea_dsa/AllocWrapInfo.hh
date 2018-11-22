c90c22167bf14b18d1e5a5f932531a4ef4d3a1b6
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2018-11-21 22:22:56 -0500

    AllocWrapInfo pass to discover malloc/free wrappers
    Based on AllocatorIdentification from llvm-dsa
    
    A function is an allocator wrapper if it is either a known
    memory allocator or calls a known memory allocator and
    returns newly allocated memory without storing it anywhere else.
    
    The root allocators are identified by name (e.g., malloc()) and using
    llvm::isAllocationFn()
    
    Names of additional allocators can be passed on command line
    
    Deallocators are handled similarly. However, there is no support for
    them in MemoryBuiltins in LLVM. Thus, they are identified by name only.

---
+++ include/sea_dsa/AllocWrapInfo.hh
@@ -0,0 +1,49 @@
+/**
+   A pass to identify alloc/dealloc function wrappers. Based on
+   AllocatorIdentification.h from llvm-dsa
+ */
+#pragma once
+#include "llvm/Pass.h"
+#include "llvm/ADT/StringRef.h"
+
+#include<set>
+#include<string>
+
+namespace llvm {
+    class Function;
+    class Module;
+    class Instruction;
+    class TargetLibraryInfo;
+    class Value;
+}
+namespace sea_dsa {
+    class AllocWrapInfo : public llvm::ModulePass {
+    protected:
+        std::set<std::string> m_allocs;
+        std::set<std::string> m_deallocs;
+
+        llvm::TargetLibraryInfo *m_tli;
+
+        void findAllocs(llvm::Module &M);
+        bool findWrappers(llvm::Module &M, std::set<std::string> &fn_names);
+        bool flowsFrom(llvm::Value*, llvm::Value*);
+    public:
+        static char ID;
+        AllocWrapInfo() : ModulePass(ID), m_tli(nullptr) {}
+
+        bool runOnModule(llvm::Module&) override;
+        llvm::StringRef getPassName() const override {
+            return "Find malloc/free wrapper functions";
+        }
+        void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
+
+        bool isAllocWrapper(llvm::Function&) const;
+        bool isDeallocWrapper(llvm::Function&) const;
+
+        const llvm::TargetLibraryInfo &getTLI() const {
+            assert(m_tli);
+            return *m_tli;
+        }
+
+    };
+}
