adb9bb76c4482f51d9ccbaf0814410693e23c3c2
Jakub Kuderski <kubakuderski@gmail.com>
2018-11-24 17:54:45 -0500

    Cleanup stats. Count fields.

--- src/DsaStats.cc
+++ src/DsaStats.cc
@@ -14,72 +14,91 @@ namespace sea_dsa {
 
   typedef DsaInfo::live_nodes_const_range nodes_range;
   typedef DsaInfo::alloc_sites_set alloc_sites_set;
-  
-  static void printMemAccesses (nodes_range nodes, llvm::raw_ostream &o)  {
+
+  static void printMemAccesses(nodes_range nodes, llvm::raw_ostream &o) {
     // Here counters
     unsigned int total_accesses = 0; // count the number of total memory accesses
-    
+
     o << " --- Memory access information\n";
     for (auto const &n: nodes) { total_accesses += n.getAccesses(); }
-    
+
     // ufo::Stats::uset ("DsaNumOfNodes", std::distance (nodes.begin () , nodes.end ()));
-    
-    o << "\t" << std::distance(nodes.begin(), nodes.end())  
+
+    o << "\t" << std::distance(nodes.begin(), nodes.end())
       << " number of read or modified nodes.\n"
-      << "\t" << total_accesses 
+      << "\t" << total_accesses
       << " number of non-trivial memory accesses"
-      << " (load/store/memcpy/memset/memmove).\n";     
-    
+      << " (load/store/memcpy/memset/memmove).\n";
+
     //  --- Print a summary of accesses
-    std::vector<NodeWrapper> sorted_nodes (nodes.begin(), nodes.end());
+    std::vector<NodeWrapper> sorted_nodes(nodes.begin(), nodes.end());
     unsigned int summ_size = 5;
-    o << "\t Summary of the " << summ_size  << " nodes with more accesses:\n";
-    std::sort (sorted_nodes.begin (), sorted_nodes.end (),
-	       [](const NodeWrapper &n1, const NodeWrapper &n2)
-	       { return (n1.getAccesses() > n2.getAccesses());});
-    
+    o << "\t Summary of the " << summ_size << " nodes with more accesses:\n";
+    std::sort(sorted_nodes.begin(), sorted_nodes.end(),
+              [](const NodeWrapper &n1, const NodeWrapper &n2) {
+                return (n1.getAccesses() > n2.getAccesses());
+              });
+
     if (total_accesses > 0) {
       for (const auto &n: sorted_nodes) {
-	if (summ_size <= 0) break;
-	summ_size--;
-	if (n.getAccesses() == 0) break;
-	o << "\t  [Node Id " << n.getId()  << "] " 
-	  << n.getAccesses() << " (" 
-	  << (int) (n.getAccesses() * 100 / total_accesses) 
-	  << "%) of total memory accesses\n" ;
+        if (summ_size <= 0) break;
+        summ_size--;
+        if (n.getAccesses() == 0) break;
+        o << "\t  [Node Id " << n.getId() << "] "
+          << n.getAccesses() << " ("
+          << (int) (n.getAccesses() * 100 / total_accesses)
+          << "%) of total memory accesses\n";
       }
     }
   }
 
-  static void printMemTypes (nodes_range nodes, llvm::raw_ostream &o) { 
+  static void printMemTypes(nodes_range nodes, llvm::raw_ostream &o) {
     // Here counters
     unsigned num_offset_collapses = 0;    // number of offset-collapsed nodes
-    unsigned num_typed_nodes = 0;  // number of typed nodes
+    unsigned num_typed_nodes = 0;         // number of typed nodes
     unsigned num_untyped_nodes = 0;
-    
-    o << " --- Type information\n";
+    unsigned num_fields = 0;
+    unsigned num_nodes_multi_typed_fields = 0;
+
+    o << " --- Node information\n";
     for (const auto &n: nodes) {
-      if (n.getNode()->isOffsetCollapsed ())
-	num_offset_collapses++;
-      else if (std::distance(n.getNode()->types().begin(),
-			     n.getNode()->types().end()) == 0)
-	num_untyped_nodes++;
-      else if (std::distance(n.getNode()->types().begin(),
-			     n.getNode()->types().end()) > 0)
-	num_typed_nodes++;
+      if (n.getNode()->isOffsetCollapsed()) {
+        num_offset_collapses++;
+        continue;
+      }
+
+      if (n.getNode()->types().empty())
+        num_untyped_nodes++;
+      else
+        num_typed_nodes++;
+
+      num_fields += n.getNode()->getLinks().size();
+
+      llvm::SmallDenseSet<unsigned, 8> fields;
+      for (auto &link : n.getNode()->getLinks()) {
+        unsigned offset = link.first.getOffset();
+        if (fields.count(offset)) {
+          ++num_nodes_multi_typed_fields;
+          break;
+        }
+        fields.insert(offset);
+      }
     }
-    o << "\t" << num_typed_nodes   << " number of typed nodes.\n";
-    o << "\t" << num_untyped_nodes << " number of untyped nodes.\n";
-    o << "\t" << num_offset_collapses     << " number of offset-collapsed nodes.\n";
-    
-    // ufo::Stats::uset ("DsaNumOfCollapsedNodes", num_collapses);
+
+    const unsigned total_nodes = std::distance(nodes.begin(), nodes.end());
+    o << "\t" << total_nodes << " number of nodes\n";
+    o << "\t\t" << num_typed_nodes << " typed nodes\n";
+    o << "\t\t" << num_untyped_nodes << " untyped nodes\n";
+    o << "\t\t" << num_offset_collapses << " offset-collapsed\n";
+    o << "\t\t" << num_nodes_multi_typed_fields << " with multi-typed fields\n";
+    o << "\t" << num_fields << " number of fields\n";
 
     // TODO: print all node's types
   }
 
-  static void printAllocSites (nodes_range nodes,
-			       const alloc_sites_set& alloc_sites,
-			       llvm::raw_ostream &o) {
+  static void printAllocSites(nodes_range nodes,
+                              const alloc_sites_set &alloc_sites,
+                              llvm::raw_ostream &o) {
     /// number of nodes without allocation sites
     unsigned num_orphan_nodes = 0;
     /// number of checks belonging to orphan nodes.
@@ -89,59 +108,52 @@ namespace sea_dsa {
     /// number of nodes with allocation sites with more than one type
     unsigned num_non_singleton = 0;
     /// total number of allocation sites
-    unsigned num_alloc_sites = std::distance (alloc_sites.begin(),
-					      alloc_sites.end ());
-    
+    unsigned num_alloc_sites = std::distance(alloc_sites.begin(),
+                                             alloc_sites.end());
+
     o << " --- Allocation site information\n";
-    
+
     // iterate over all nodes and update some counters
-    for (const auto &n: nodes) { 
-      unsigned num_alloc_sites = n.getNode()->getAllocSites ().size ();
-      if (num_alloc_sites == 0)  {
-	num_orphan_nodes++;
-	num_orphan_checks += n.getAccesses();
-      } 
-      else 
-	max_alloc_sites = std::max (max_alloc_sites, num_alloc_sites);
+    for (const auto &n: nodes) {
+      unsigned num_alloc_sites = n.getNode()->getAllocSites().size();
+      if (num_alloc_sites == 0) {
+        num_orphan_nodes++;
+        num_orphan_checks += n.getAccesses();
+      } else
+        max_alloc_sites = std::max(max_alloc_sites, num_alloc_sites);
     }
 
     // count the number of nodes with more than one allocation site
-    for (const auto &n: nodes)  {
-      SmallPtrSet<Type*,32> allocTypes;
-      for (const llvm::Value*v : n.getNode ()->getAllocSites ()) 
-	allocTypes.insert(v->getType());
+    for (const auto &n: nodes) {
+      SmallPtrSet<Type *, 32> allocTypes;
+      for (const llvm::Value *v : n.getNode()->getAllocSites())
+        allocTypes.insert(v->getType());
       num_non_singleton += (allocTypes.size() > 1);
     }
 
     // ufo::Stats::uset ("DsaNumOfAllocationSites", num_alloc_sites);
-    
-    o << "\t" << num_alloc_sites  << " number of allocation sites\n";
-    o << "\t   " << max_alloc_sites  << " max number of allocation sites in a node\n";
-    o << "\t" << num_orphan_nodes  << " number of nodes without allocation site\n";
-    o << "\t" << num_orphan_checks << " number of memory accesses without allocation site\n";
-    o << "\t" << num_non_singleton << " number of nodes with multi-typed allocation sites\n";
+
+    o << "\t" << num_alloc_sites << " number of allocation sites\n";
+    o << "\t   " << max_alloc_sites
+      << " max number of allocation sites in a node\n";
+    o << "\t" << num_orphan_nodes
+      << " number of nodes without allocation site\n";
+    o << "\t" << num_orphan_checks
+      << " number of memory accesses without allocation site\n";
+    o << "\t" << num_non_singleton
+      << " number of nodes with multi-typed allocation sites\n";
   }
   
 
   void DsaPrintStats::runOnModule (Module &M)  {
+    auto dsa_nodes = m_dsa.live_nodes();
+    auto const &dsa_alloc_sites = m_dsa.alloc_sites();
     
-    unsigned num_of_funcs = 0;
-    for (auto &f: M)  {
-      if (f.isDeclaration () || f.empty ()) continue;
-      num_of_funcs++; 	
-    }
-    
-    // ufo::Stats::uset ("NumOfFunctions", num_of_funcs);
-    
-    auto dsa_nodes = m_dsa.live_nodes ();
-    auto const &dsa_alloc_sites = m_dsa.alloc_sites ();
-    
-    errs() << " ========== Begin SeaHorn Dsa info  ==========\n";
-    printMemAccesses (dsa_nodes, errs());
-    printMemTypes (dsa_nodes, errs());
-    printAllocSites  (dsa_nodes, dsa_alloc_sites, errs());
-    errs() << " ========== End SeaHorn Dsa info  ==========\n";
-    
+    errs() << " ========== Begin SeaDsa info  ==========\n";
+    printMemAccesses(dsa_nodes, errs());
+    printMemTypes(dsa_nodes, errs());
+    printAllocSites(dsa_nodes, dsa_alloc_sites, errs());
+    errs() << " ========== End SeaDsa info  ==========\n";
   }
   
 
@@ -150,16 +162,16 @@ namespace sea_dsa {
     
     static char ID;
     
-    DsaPrintStatsPass (): ModulePass (ID) {}
+    DsaPrintStatsPass(): ModulePass (ID) {}
     
-    void getAnalysisUsage (AnalysisUsage &AU) const override {
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
       AU.addRequired<DsaInfoPass>();
       AU.setPreservesAll();
     }
     
-    bool runOnModule (Module &M) override {
-      DsaPrintStats p (getAnalysis<DsaInfoPass>().getDsaInfo());
-      p.runOnModule (M);
+    bool runOnModule(Module &M) override {
+      DsaPrintStats p(getAnalysis<DsaInfoPass>().getDsaInfo());
+      p.runOnModule(M);
       return false;
     }
   
@@ -170,8 +182,8 @@ namespace sea_dsa {
 
   char DsaPrintStatsPass::ID = 0;
   
-  Pass *createDsaPrintStatsPass () {
-    return new DsaPrintStatsPass ();
+  Pass *createDsaPrintStatsPass() {
+    return new DsaPrintStatsPass();
   }
   
 } // end namespace sea_dsa
