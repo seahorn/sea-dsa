a29c452dbe02913a9ee3ec536e2784d7657129ad
Jakub Kuderski <kubakuderski@gmail.com>
2018-11-24 17:58:18 -0500

    clang-format DSA analysis

--- src/DsaAnalysis.cc
+++ src/DsaAnalysis.cc
@@ -1,13 +1,13 @@
-#include "llvm/IR/DataLayout.h"
-#include "llvm/Analysis/TargetLibraryInfo.h"
-#include "llvm/Analysis/MemoryBuiltins.h"
 #include "llvm/Analysis/CallGraph.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/Analysis/MemoryBuiltins.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/IR/DataLayout.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
 
-#include "sea_dsa/Info.hh"
-#include "sea_dsa/Global.hh"
 #include "sea_dsa/DsaAnalysis.hh"
+#include "sea_dsa/Global.hh"
+#include "sea_dsa/Info.hh"
 #include "sea_dsa/Stats.hh"
 #include "sea_dsa/support/NameValues.hh"
 #include "sea_dsa/support/RemovePtrToInt.hh"
@@ -15,78 +15,74 @@
 using namespace sea_dsa;
 using namespace llvm;
 
-static llvm::cl::opt<sea_dsa::GlobalAnalysisKind>
-DsaGlobalAnalysis ("sea-dsa",
-       llvm::cl::desc ("DSA: kind of Dsa analysis"),
-       llvm::cl::values 
-       (clEnumValN (CONTEXT_SENSITIVE  , "cs"   , "Context-sensitive (default)"),
-        clEnumValN (CONTEXT_INSENSITIVE, "ci"   , "Context-insensitive"),
-        clEnumValN (FLAT_MEMORY        , "flat" , "Flat memory")),
-       llvm::cl::init (CONTEXT_SENSITIVE));
+static llvm::cl::opt<sea_dsa::GlobalAnalysisKind> DsaGlobalAnalysis(
+    "sea-dsa", llvm::cl::desc("DSA: kind of Dsa analysis"),
+    llvm::cl::values(
+        clEnumValN(CONTEXT_SENSITIVE, "cs", "Context-sensitive (default)"),
+        clEnumValN(CONTEXT_INSENSITIVE, "ci", "Context-insensitive"),
+        clEnumValN(FLAT_MEMORY, "flat", "Flat memory")),
+    llvm::cl::init(CONTEXT_SENSITIVE));
 
 static llvm::cl::opt<bool>
-DsaStats("sea-dsa-stats",
-	llvm::cl::desc("Print stats about Dsa analysis"),
-	llvm::cl::init (false));
+    DsaStats("sea-dsa-stats", llvm::cl::desc("Print stats about Dsa analysis"),
+             llvm::cl::init(false));
 
-		     
-void DsaAnalysis::getAnalysisUsage (AnalysisUsage &AU) const {
-  AU.addRequired<TargetLibraryInfoWrapperPass> ();
-  AU.addRequired<CallGraphWrapperPass> ();
+void DsaAnalysis::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.addRequired<TargetLibraryInfoWrapperPass>();
+  AU.addRequired<CallGraphWrapperPass>();
   // XXX: needed if we run later DsaInfo
   AU.addRequired<RemovePtrToInt>();
   AU.addRequired<NameValues>();
-  AU.setPreservesAll ();
+  AU.setPreservesAll();
 }
 
-const DataLayout& DsaAnalysis::getDataLayout () {
-  assert (m_dl);
+const DataLayout &DsaAnalysis::getDataLayout() {
+  assert(m_dl);
   return *m_dl;
 }
 
-const TargetLibraryInfo& DsaAnalysis::getTLI () {
-  assert (m_tli);
+const TargetLibraryInfo &DsaAnalysis::getTLI() {
+  assert(m_tli);
   return *m_tli;
 }
 
-GlobalAnalysis& DsaAnalysis::getDsaAnalysis () {
-  assert (m_ga);
+GlobalAnalysis &DsaAnalysis::getDsaAnalysis() {
+  assert(m_ga);
   return *m_ga;
 }
 
-bool DsaAnalysis::runOnModule (Module &M) {
-  m_dl  = &M.getDataLayout ();
-  m_tli = &getAnalysis<TargetLibraryInfoWrapperPass> ().getTLI();
-  auto &cg = getAnalysis<CallGraphWrapperPass> ().getCallGraph ();
+bool DsaAnalysis::runOnModule(Module &M) {
+  m_dl = &M.getDataLayout();
+  m_tli = &getAnalysis<TargetLibraryInfoWrapperPass>().getTLI();
+  auto &cg = getAnalysis<CallGraphWrapperPass>().getCallGraph();
 
   switch (DsaGlobalAnalysis) {
   case CONTEXT_INSENSITIVE:
-    m_ga.reset (new ContextInsensitiveGlobalAnalysis
-		(*m_dl, *m_tli, cg, m_setFactory, false));
+    m_ga.reset(new ContextInsensitiveGlobalAnalysis(*m_dl, *m_tli, cg,
+                                                    m_setFactory, false));
     break;
   case FLAT_MEMORY:
-    m_ga.reset (new ContextInsensitiveGlobalAnalysis
-		(*m_dl, *m_tli, cg, m_setFactory, true /* use flat*/));
+    m_ga.reset(new ContextInsensitiveGlobalAnalysis(
+        *m_dl, *m_tli, cg, m_setFactory, true /* use flat*/));
     break;
-  default: /* CONTEXT_SENSITIVE */    
-    m_ga.reset (new ContextSensitiveGlobalAnalysis
-		(*m_dl, *m_tli, cg, m_setFactory));
+  default: /* CONTEXT_SENSITIVE */
+    m_ga.reset(
+        new ContextSensitiveGlobalAnalysis(*m_dl, *m_tli, cg, m_setFactory));
   }
-  
-  m_ga->runOnModule (M);
+
+  m_ga->runOnModule(M);
 
   if (DsaStats) {
-    DsaInfo i (*m_dl, *m_tli,getDsaAnalysis ());
-    i.runOnModule (M);
-    DsaPrintStats p (i);
-    p.runOnModule (M);
+    DsaInfo i(*m_dl, *m_tli, getDsaAnalysis());
+    i.runOnModule(M);
+    DsaPrintStats p(i);
+    p.runOnModule(M);
   }
-    
+
   return false;
 }
 
-
 char DsaAnalysis::ID = 0;
 
-static llvm::RegisterPass<DsaAnalysis> 
-X ("seadsa", "Entry point for all SeaHorn Dsa clients");
+static llvm::RegisterPass<DsaAnalysis>
+    X("seadsa", "Entry point for all SeaHorn Dsa clients");
