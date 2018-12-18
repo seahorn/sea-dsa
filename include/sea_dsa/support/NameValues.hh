b854fe8bb6acf621e6c7dd2b9de5f69b1911d6c0
Jakub Kuderski <kubakuderski@gmail.com>
2018-12-18 10:00:55 -0500

    Clang-format NameValues pass

--- include/sea_dsa/support/NameValues.hh
+++ include/sea_dsa/support/NameValues.hh
@@ -1,24 +1,22 @@
 #ifndef __SEA_DSA_NAME_VALUES__HPP_
 #define __SEA_DSA_NAME_VALUES__HPP_
 
-#include "llvm/Pass.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Function.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
 
 namespace sea_dsa {
-  struct NameValues : public llvm::ModulePass {
-    static char ID;
-    NameValues () : llvm::ModulePass (ID) {}
-    bool runOnModule (llvm::Module &M);
-    bool runOnFunction (llvm::Function &F);
-    void getAnalysisUsage (llvm::AnalysisUsage &AU) const {
-      AU.setPreservesAll ();
-    }
-    virtual llvm::StringRef getPassName () const {
-      return "sea-dsa: names all unnamed values";
-    }
-  };
-}
+struct NameValues : public llvm::ModulePass {
+  static char ID;
+  NameValues() : llvm::ModulePass(ID) {}
+  bool runOnModule(llvm::Module &M);
+  bool runOnFunction(llvm::Function &F);
+  void getAnalysisUsage(llvm::AnalysisUsage &AU) const { AU.setPreservesAll(); }
+  virtual llvm::StringRef getPassName() const {
+    return "sea-dsa: names all unnamed values";
+  }
+};
+} // namespace sea_dsa
 
 #endif
