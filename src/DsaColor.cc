b89da1e7b371eb91abe3bdbc3d52dbca75c206fe
caballa <navasjorgea@gmail.com>
2020-02-11 20:19:27 -0500

    Remove warnings for unused variables

--- src/DsaColor.cc
+++ src/DsaColor.cc
@@ -29,9 +29,8 @@ void color_nodes_aux(const Node &n_callee, const Node &n_caller,
   c_callee.insert({&n_callee, col});
 
   for (auto &links : n_callee.getLinks()) {
-    const Field &f = links.first;
     const Cell &next_c_callee = *links.second;
-    const Node * next_n_callee = next_c_callee.getNode();
+    const Node *next_n_callee = next_c_callee.getNode();
     const Cell &next_c_caller = sm.get(next_c_callee);
     const Node *next_n_caller = next_c_caller.getNode();
 
@@ -64,7 +63,7 @@ void colorGraph(const DsaCallSite &cs, const Graph &calleeG,
 
   SimulationMapper simMap;
 
-  bool res = Graph::computeCalleeCallerMapping(
+  Graph::computeCalleeCallerMapping(
       cs, *(const_cast<Graph *>(&calleeG)), *(const_cast<Graph *>(&callerG)),
       simMap);
 
--- src/ShadowMem.cc
+++ src/ShadowMem.cc
@@ -432,7 +432,6 @@ class ShadowMemImpl : public InstVisitor<ShadowMemImpl> {
 
   void markUseCall(CallInst *ci, llvm::Optional<unsigned> accessedBytes) {
     assert(m_llvmCtx);
-    Module *m = ci->getModule();
     MDNode *meta = MDNode::get(*m_llvmCtx, None);
     ci->setMetadata(m_metadataTag, meta);
     ci->setMetadata(m_memUseTag, mkMetaConstant(accessedBytes));
@@ -834,7 +833,6 @@ bool ShadowMemImpl::runOnFunction(Function &F) {
   this->visit(F);
 
   auto &B = *m_B;
-  auto &ctx = *m_llvmCtx;
   auto &G = *m_graph;
   // -- compute pseudo-functions for inputs and outputs
 
@@ -1082,8 +1080,10 @@ void ShadowMemImpl::visitDsaCallSite(dsa::DsaCallSite &CS) {
     dsa::Cell callerC = simMap.get(dsa::Cell(const_cast<dsa::Node *>(n), 0));
     assert(!callerC.isNull() && "Not found node in the simulation map");
 
-    AllocaInst *v = getShadowForField(callerC);
-    unsigned id = getFieldId(callerC);
+    // allocate shadow for callerC
+    getShadowForField(callerC);
+    // assign an id to callerC
+    getFieldId(callerC);
 
     // -- read only node ignore nodes that are only reachable
     // -- from the return of the function
@@ -1354,9 +1354,11 @@ CallInst &ShadowMemImpl::getParentDef(CallInst &memOp) {
   if (isMemInit(memOp))
     return memOp;
 
-  auto *fn = memOp.getCalledFunction();
-  assert(fn);
-  assert(fn == m_memLoadFn || fn == m_memStoreFn || fn == m_memTrsfrLoadFn);
+  if (auto *fn = memOp.getCalledFunction()) {
+    assert(fn == m_memLoadFn || fn == m_memStoreFn || fn == m_memTrsfrLoadFn);
+  } else {
+    assert(false && "CallInst should be a shadow mem call");
+  }
 
   assert(memOp.getNumOperands() >= 1);
   Value *defArg = memOp.getOperand(1);
