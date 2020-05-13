77eb434f739e43006fb675083f236f410da9b291
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-05-13 11:34:42 -0400

    style: DOG() macro and LOG() macro formatting

--- include/seadsa/support/Debug.h
+++ include/seadsa/support/Debug.h
@@ -1,20 +1,24 @@
 #pragma once
-#include <string>
 #include <set>
+#include <string>
 
-#define LOG(TAG,CODE) \
-  do { if(::seadsa::SeaDsaLog.count(TAG) > 0) {CODE;} else {;} } while(false)
+#define LOG(TAG, CODE)                                                         \
+  do {                                                                         \
+    if (::seadsa::SeaDsaLog.count(TAG) > 0) {                                  \
+      CODE;                                                                    \
+    } else {                                                                   \
+      ;                                                                        \
+    }                                                                          \
+  } while (false)
 
+#define DOG(CODE) LOG(DEBUG_TYPE, CODE)
 
 namespace seadsa {
 
-  extern std::set<std::string> SeaDsaLog;
-  void SeaDsaEnableLog (std::string x);
+extern std::set<std::string> SeaDsaLog;
+void SeaDsaEnableLog(std::string x);
 
-  struct SeaDsaLogOpt {
-    void operator=(const std::string &tag) const {
-      SeaDsaEnableLog(tag);
-    }
-  };
-}
-
+struct SeaDsaLogOpt {
+  void operator=(const std::string &tag) const { SeaDsaEnableLog(tag); }
+};
+} // namespace seadsa
