f96f8ed0705915e5ff8614e4ab079c2b4ae84779
Anton <avvassil@uwaterloo.ca>
2020-05-20 16:33:50 -0400

    unify naming convention

--- include/seadsa/sea_dsa.h
+++ include/seadsa/sea_dsa.h
@@ -8,7 +8,7 @@ extern "C" {
 #endif
   
 // sea-dsa will unify all argument's cells
-extern void seadsa_alias(const void *p, ...);
+extern void sea_dsa_alias(const void *p, ...);
 // sea-dsa will mark the node pointed to by p as read (R)
 extern void sea_dsa_set_read(const void *p);
 // sea-dsa will mark the node pointed to by p as modified (M)
@@ -16,13 +16,13 @@ extern void sea_dsa_set_modified(const void *p);
 // sea-dsa will mark the node pointed to by p as ptr to int (2)
 extern void sea_dsa_set_ptrtoint(const void *p);
 // sea-dsa will collapse the argument's cell
-extern void seadsa_collapse(const void *p);
+extern void sea_dsa_collapse(const void *p);
 // sea-dsa will return a fresh memory object
-extern void* seadsa_new();
+extern void* sea_dsa_new();
 // sea-dsa will mark the node pointed by p as a sequence node of size sz
 // The noded pointed by p cannot be already a sequence node and its
 // size must be less or equal than sz.
-extern void seadsa_mk_seq(const void *p, unsigned sz);
+extern void sea_dsa_mk_seq(const void *p, unsigned sz);
 #ifdef __cplusplus
 }
 #endif
