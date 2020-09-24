c3b88ab894010b3460ea83fec32f823a78d930e3
caballa <navasjorgea@gmail.com>
2020-09-24 13:51:52 -0500

    fix(shadowmem): revert partially commit a393e0e.
    This commit tried to fix the problem with issue #102 where we created
    a shadow.mem.out annotation also if a region is read-only. However,
    the fix in commit a393e0e was too relaxed because created a region
    even if the region is not accessed at all. This creates the problem
    described in issue #104. The solution that fixes both issues #102 and
    #104 is to create a shadow.mem.out if the region is read or modified.

--- lib/seadsa/ShadowMem.cc
+++ lib/seadsa/ShadowMem.cc
@@ -916,7 +916,9 @@ bool ShadowMemImpl::runOnFunction(Function &F) {
     }
 
     /// final value
-    mkMarkOut(B, c, idx, llvm::None);
+    if (isRead(n, F) || isModified(n, F)) {    
+      mkMarkOut(B, c, idx, llvm::None);
+    }
   };
 
   unsigned idx = 0;
@@ -1087,7 +1089,7 @@ void ShadowMemImpl::visitDsaCallSite(dsa::DsaCallSite &CS) {
       // Unclear how to get the associated concrete pointer here.
     }
     // -- read/write or new node
-    else {
+    else if (isRead(CN, CF) || isModified(CN,CF)) {
       // -- n is new node iff it is reachable only from the return node
       Constant *argFn = isReturned(CN) ? m_argNewFn : m_argModFn;
       mkArgNewMod(*m_B, argFn, callerC, idx, llvm::None);
