dbfc8e117f723d229d16c17b315ad13be4866348
caballa <navasjorgea@gmail.com>
2021-11-11 11:49:05 +0100

    style: clang-format

--- include/seadsa/Printer.hh
+++ include/seadsa/Printer.hh
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "llvm/IR/Module.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/IR/Module.h"
 
 #include "seadsa/CompleteCallGraph.hh"
 #include "seadsa/Global.hh"
@@ -12,39 +12,39 @@ namespace seadsa {
 
 class DsaPrinterImpl;
 
-/** 
+/**
  * Print seadsa graphs in dot format.
- * 
+ *
  * If the analysis is context-sensitive then the _global_ seadsa graph
  * of each function FUN is printed in the file FUN.mem.dot. If the
  * analysis is context-insensitive then only one _global_ seadsa graph
  * is printed for the main function. By global, we mean the results of
- * the corresponding analysis upon completion. In addition, 
+ * the corresponding analysis upon completion. In addition,
  * summaries graphs are printed if printSummaryGraphs is enabled.
  **/
 class DsaPrinter {
   std::unique_ptr<DsaPrinterImpl> m_impl;
+
 public:
   DsaPrinter(GlobalAnalysis &dsa, CompleteCallGraph *ccg,
-	     bool printSummaryGraphs = false);
+             bool printSummaryGraphs = false);
 
   ~DsaPrinter();
 
   bool runOnModule(llvm::Module &M);
 };
 
-
 class DsaPrinterPass : public llvm::ModulePass {
-public :
+public:
   static char ID;
-  
+
   DsaPrinterPass();
 
   bool runOnModule(llvm::Module &M) override;
 
   void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
-  
+
   llvm::StringRef getPassName() const override;
 };
 
-} //end namespace seadsa
+} // end namespace seadsa
