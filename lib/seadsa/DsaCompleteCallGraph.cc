5dfafb6e5978e75f7166e5e16bbc0879fbd206f7
caballa <navasjorgea@gmail.com>
2022-01-28 14:56:24 +0100

    fix(callgraph): avoid assertion failure
    This fix avoids an assertion in CallGraph::operator[] to fail while
    checking that the function is in the callgraph.

--- lib/seadsa/DsaCompleteCallGraph.cc
+++ lib/seadsa/DsaCompleteCallGraph.cc
@@ -588,9 +588,10 @@ bool CompleteCallGraphAnalysis::runOnModule(Module &M) {
   /// Remove edges in the callgraph: remove original indirect call
   /// from call graph if we now for sure we fully resolved it.
   for (auto &F : M) {
-    if (isDbgInfoIntrinsic(F.getIntrinsicID())) continue;
-
+    if (F.empty()) continue;
+    
     CallGraphNode *CGNF = (*m_complete_cg)[&F];
+    assert(CGNF);
     if (!CGNF) continue;
 
     // collect first callsites to avoid invalidating iterators
