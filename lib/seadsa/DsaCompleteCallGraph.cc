dbd283c44b7b297f2900697ee25cbca4598110c6
Adrian Herrera <adrian.herrera02@gmail.com>
2022-03-16 14:03:23 +1100

    fix: skip debug functions in the call graph

--- lib/seadsa/DsaCompleteCallGraph.cc
+++ lib/seadsa/DsaCompleteCallGraph.cc
@@ -7,6 +7,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Pass.h"
@@ -646,6 +647,8 @@ bool CompleteCallGraphAnalysis::runOnModule(Module &M) {
   /// Remove edges in the callgraph: remove original indirect call
   /// from call graph if we know for sure we fully resolved it.
   for (auto &F : M) {
+    if (isDbgInfoIntrinsic(F.getIntrinsicID())) continue;
+
     if (F.empty()) continue;
     
     CallGraphNode *CGNF = (*m_complete_cg)[&F];
