86f92615523593c86aa49ddfe84cfb66e27c1b61
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2022-09-20 15:46:11 -0400

    chore: silence compiler warnings

--- lib/seadsa/Graph.cc
+++ lib/seadsa/Graph.cc
@@ -401,6 +401,7 @@ const Cell &Node::getLink(Field _f) const {
   if (m_links.count(omni)) return *m_links.at(omni);
 
   assert(false); // unreachable
+  llvm_unreachable();
 }
 
 void Node::setLink(const Field _f, const Cell &c) {
